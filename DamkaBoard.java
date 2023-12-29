0bded6c1cb407e325838931b8c7034292ad3fc5b
amitfrumkin <amit.frumkin@post.runi.ac.il>
2023-12-29 18:23:30 +0200

    correction to damka2

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -8,15 +8,13 @@ public class DamkaBoard {
 		int n = Integer.parseInt(args[0]) ;
 		
 
-		for (int i = 1 ; i<= n ; i++){	
-			System.out.println(" ") ;
-			for(int j = 1 ; j <= n+n ; j++){
-				if((j+i)%2 == 0){
+		for (int i = 0 ; i < n ; i++){	
+			System.out.println() ;
+			for(int j = 0 ; j < n ; j++){
+				if(i%2 == 0){
 					System.out.print("* ") ;
-				}
-		
-				else{
-					System.out.print("  ") ;
+				}else{
+					System.out.print(" *") ;
 				}
 				
 			}
